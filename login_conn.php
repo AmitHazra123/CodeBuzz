8587f40caac25bb696d5ba4e2e2fcba3acf879da
Amit Hazra <shivahazra@gmail.com>
2018-06-21 03:32:53 +0530

    Fifth Commit

--- login_conn.php
+++ login_conn.php
@@ -1,18 +1,18 @@
 <?php
 	if($_POST['username'] && $_POST['pass']){
-		$user=$_POST['username'];
-		$pass=$_POST['pass'];
+		$user = $_POST['username'];
+		$password =$_POST['pass'];
 
 		$clients=array(array('user'=>'AMIT','pass'=>'amit123'),array('user'=>'SYED','pass'=>'SYED007'),array('user'=>'SAYAN','pass'=>'sayan969'));
 
-		if($user=='admin@codebuzz.com' && $pass=='admin'){
+		if($user=='admin@codebuzz.com' && $password =='admin'){
 			session_start();
 			$_SESSION['id'] = 1;
 			header("location:admin.php");
 		}
 		else {
 			foreach($clients as $client){
-				if($cilent['user']==$user && $client['pass'] == $pass){
+				if($cilent['user'] == $user && $client['pass'] == $password){
 					session_start();
 					$_SESSION['id'] = 1;
 					header("location:index.php");
