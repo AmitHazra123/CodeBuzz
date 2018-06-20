5da801ee4ad39ee3852303193c2e244760e5b8e4
Amit Hazra <shivahazra@gmail.com>
2018-06-21 02:26:29 +0530

    Fifth Commit

--- login.php
+++ login.php
@@ -58,9 +58,7 @@
 					<div class="container-login100-form-btn">
 						<div class="wrap-login100-form-btn">
 							<div class="login100-form-bgbtn"></div>
-							<button class="login100-form-btn">
-								Login
-							</button>
+							<input type="submit" value="Login" class="login100-form-btn" />
 						</div>
 					</div>
 
